04a9c56c4ad4be4a8fe595624672e141a2072a31
Alvaro Miranda Aguilera <kikitux@gmail.com>
2018-10-08 17:16:50 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,5 @@
-variable "word" {
-  description = "a variable to be used on the privisioning"
-}
-
 resource "null_resource" "helloWorld" {
   provisioner "local-exec" {
     command = "echo hello world"
-  }
-}
-
-resource "null_resource" "helloVar" {
-  provisioner "local-exec" {
-    command = "echo hello ${var.word}"
   }
 }
