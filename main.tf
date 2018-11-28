1be8ac664ddddd1f81909295532c0e86a3237d01
Alvaro Miranda Aguilera <kikitux@gmail.com>
2018-11-28 09:51:54 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,14 @@
-resource "null_resource" "helloWorld" {
-  provisioner "local-exec" {
-    command = "echo hello world"
-  }
-  
+resource "random_pet" "mypet" {
   keepers = {
     timestamp = "${timestamp()}"
   }
-  
+
+  separator = " "
+}
+
+resource "null_resource" "helloWorld" {
+  provisioner "local-exec" {
+    command = "echo hello ${random_pet.mypet.id}"
+  }
+ 
 }
